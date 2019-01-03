d56324b54a6666f62200667bc89f6d7585f53e92
Zqw <578159259@qq.com>
2019-01-03 17:00:06 +0800

    修改个人信息

--- HomeSchoolInteraction/js/utils/UploadHeadImage.js
+++ HomeSchoolInteraction/js/utils/UploadHeadImage.js
@@ -337,16 +337,23 @@ var UploadHeadImage = (function($, mod) {
 		//不需要加密的数据
 		var comData0 = {
 			uuid: publicParameter.uuid, //用户设备号
-			utid: personal.utid, //用户ID
 			type: 'uico', //修改类型，upw:密码, uico:头像
 			val: domain, //对应类型的值
 			utoken: personal.utoken, //用户令牌
 			appid: publicParameter.appid //系统所分配的应用ID
-		};
+		}
+		var tempUrl = '';
+		if (personal.utp == 1) { //0老师，2学生，1家长
+			comData0.utid = personal.utid;
+			tempUrl = 'UpUserInfo';
+		} else{
+			comData0.stuid = personal.utid;
+			tempUrl = 'UpStuInfo';
+		}
 		events.showWaiting();
 		//发送网络请求，data为网络返回值
-		postDataEncry('UpUserInfo', enData0, comData0, 0, function(data) {
-			console.log('UpUserInfo:' + JSON.stringify(data));
+		postDataEncry(tempUrl, enData0, comData0, 0, function(data) {
+			console.log(tempUrl + JSON.stringify(data));
 			events.closeWaiting();
 			if(data.RspCode == 0) {
 				//成功的回调
